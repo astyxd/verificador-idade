5c4e7aa681d35d524d3db974ba1bde47ec43d87e
asty <104666282+astyxd@users.noreply.github.com>
2022-05-10 18:05:24 -0300

    att

---
+++ script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERROR] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gen = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gen = 'Homem'
+            if (idade > 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-m.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-m.jpg')
+            }
+        } else if (fsex[1].checked) {
+            gen = 'Mulher'
+            if (idade > 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gen} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
